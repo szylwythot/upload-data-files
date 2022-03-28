8fc994448e9cccd2d5204d4d4ac86e070ff5da91
toth_szilvia <szylwy@gmail.com>
2022-03-28 14:26:57 +0200

    basic server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { appendFile } = require("fs");
+const path = require("path");
+const app = express();
+
+function getFunction(request, response){
+  response.sendFile(
+    path.join(`${__dirname}/../frontend/index.html`));
+}
+
+app.get("/", getFunction);
+app.use("/pub", express.static(`${__dirname}/../frontend/public`));
+
+const port = 9000;
+const ipAddress = `http://127.0.0.1:${port}`
+app.listen(port, () => {
+    console.log(ipAddress)
+});
